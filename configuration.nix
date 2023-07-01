16216444d704cb102e25177bd6fe0b84ef42b956
YMSTNT <21342713+YMSTNT@users.noreply.github.com>
2023-07-02 00:07:48 +0200

    Create configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,211 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Setup keyfile
+  boot.initrd.secrets = {
+    "/crypto_keyfile.bin" = null;
+  };
+
+  # Enable swap on luks
+  boot.initrd.luks.devices."luks-29992459-3785-432a-9691-52ced97012df".device = "/dev/disk/by-uuid/29992459-3785-432a-9691-52ced97012df";
+  boot.initrd.luks.devices."luks-29992459-3785-432a-9691-52ced97012df".keyFile = "/crypto_keyfile.bin";
+
+  networking.hostName = "ymstnt-desktop"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Budapest";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "hu_HU.UTF-8";
+    LC_IDENTIFICATION = "hu_HU.UTF-8";
+    LC_MEASUREMENT = "hu_HU.UTF-8";
+    LC_MONETARY = "hu_HU.UTF-8";
+    LC_NAME = "hu_HU.UTF-8";
+    LC_NUMERIC = "hu_HU.UTF-8";
+    LC_PAPER = "hu_HU.UTF-8";
+    LC_TELEPHONE = "hu_HU.UTF-8";
+    LC_TIME = "hu_HU.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "hu";
+    xkbVariant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "hu101";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # NTFS
+  boot.supportedFilesystems = [ "ntfs" ];
+
+  # Nvidia
+  # Make sure opengl is enabled
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  # Tell Xorg to use the nvidia driver
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    # Modesetting is needed for most wayland compositors
+    modesetting.enable = true;
+    # Use the open source version of the kernel module
+    # Only available on driver 515.43.04+
+    open = true;
+    # Enable the nvidia settings menu
+    nvidiaSettings = true;
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Enable flatpak
+  services.flatpak.enable = true;
+  
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.ymstnt = {
+    isNormalUser = true;
+    description = "YMSTNT";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      firefox
+      brave
+      thunderbird
+      discord
+      obsidian
+      onlyoffice-bin
+      vscodium
+      syncthing
+      evolution
+      gnome.gnome-tweaks
+      gnome.dconf-editor
+      gnome.gnome-software
+      cryptomator
+      gimp
+      pika-backup
+      steam
+    ];
+  };
+
+  # Mod Discord with Vencord and OpenAsar
+  nixpkgs.overlays =
+   let
+     myOverlay = self: super: {
+       discord = super.discord.override { withOpenASAR = true; };
+     };
+   in
+   [ myOverlay ];
+   
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    micro
+    pfetch
+    nushell
+    starship
+    adw-gtk3
+  ];
+
+  # GNOME debloat
+  environment.gnome.excludePackages = with pkgs.gnome; [
+    cheese      # photo booth
+    epiphany    # web browser
+    totem       # video player
+    yelp        # help viewer
+    geary       # email client
+    gnome-maps
+    gnome-music
+    pkgs.gnome-photos
+    pkgs.gnome-connections
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+}
