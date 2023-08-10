68292e5fd26d6a5f945e993033221ba6d2243ac2
YMSTNT <21342713+YMSTNT@users.noreply.github.com>
2023-08-10 21:42:19 +0200

    Update nix configuration

--- home.nix
+++ home.nix
@@ -23,8 +23,10 @@
           shellAliases = {
             ll = "ls -l";
             update = "sudo nixos-rebuild switch";
+            channelupd = "sudo nix-channel --update";
             nixconfig = "sudo micro /etc/nixos/configuration.nix";
             homeconfig = "sudo micro /etc/nixos/home.nix";
+            deletegarbage = "sudo nix-collect-garbage --delete-older-than 30d";
             zshreload = "source ~/.zshrc";
             lf = "ls -la | grep";
             ff = "find | grep";
@@ -49,51 +51,93 @@
             .  ~/.zshrc_old
           '';
        };
-	};
 
-	programs.starship = {
-		enable = true;
-		settings = {
-			format = "[](\#003F5E)\$os\$username\[](bg:\#005077 fg:\#003F5E)\$directory\[](bg:\#006496 fg:\#005077)\$git_branch\$git_status\[ ](fg:\#006496)";
+       programs.starship = {
+     	 enable = true;
+       	   settings = {
+       		 format = "[](\#003F5E)\$os\$username\[](bg:\#005077 fg:\#003F5E)\$directory\[](bg:\#006496 fg:\#005077)\$git_branch\$git_status\[ ](fg:\#006496)";
+       
+       		 username = {
+       		   show_always = true;
+       		   style_user = "bg:\#003F5E";
+       		   style_root = "bg:\#003F5E";
+       		   format = "[$user ]($style)";
+       		   disabled = false;
+       		 };
+       
+       		 os = {
+       		   format = "[ ]($style)";
+       		   style = "bg:\#003F5E";
+       		   disabled = false;
+       	     };
+       
+       			directory = {
+       			  style = "bg:\#005077";
+       			  format = "[ $path ]($style)";
+       			  truncation_length = 3;
+       			  truncation_symbol = "…/";
+       			};
+       
+       			directory.substitutions = {
+       			  "Documents" = " ";
+       			  "Downloads" = " ";
+       			  "Music" = " ";
+       			  "Pictures" = " ";
+       			};
+       
+       			git_branch = {
+       			  symbol = "";
+       			  style = "bg:\#006496";
+       			  format = "[ $symbol $branch ]($style)";
+       			};
+       
+       			git_status = {
+       			  style = "bg:\#006496";
+       			  format = "[$all_status$ahead_behind ]($style)";
+       			};
+       		};
+       	};
 
-			username = {
-			  show_always = true;
-			  style_user = "bg:\#003F5E";
-			  style_root = "bg:\#003F5E";
-			  format = "[$user ]($style)";
-			  disabled = false;
-			};
+       	dconf.settings = {
+       	  "org/gnome/shell" = {
+             disable-user-extensions = false;
 
-			os = {
-			  format = "[ ]($style)";
-			  style = "bg:\#003F5E";
-			  disabled = false;
-			};
+             enabled-extensions = [
+               "just-perfection-desktop@just-perfection"
+               "draw-on-your-screen2@zhrexl.github.com"
+               "activities-filled-pill@verdre"
+               "extensions-sync@elhan.io"
+             ];
+       	  
+       	  	 favorite-apps = [
+       	  	   "brave-browser.desktop"
+       	  	   "thunderbird.desktop"
+       	  	   "discord.desktop"
+       	  	   "com.raggesilver.BlackBox.desktop"
+       	  	   "org.gnome.Nautilus.desktop" 	
+       	  	 ];
+       	   };
+       	   "org/gnome/desktop/interface" = {
+       	      color-scheme = "prefer-dark";
+       	      gtk-theme = "adw-gtk3-dark";
+       	   };
+       	};
 
-			directory = {
-			  style = "bg:\#005077";
-			  format = "[ $path ]($style)";
-			  truncation_length = 3;
-			  truncation_symbol = "…/";
-			};
-
-			directory.substitutions = {
-			  "Documents" = " ";
-			  "Downloads" = " ";
-			  "Music" = " ";
-			  "Pictures" = " ";
-			};
-
-			git_branch = {
-			  symbol = "";
-			  style = "bg:\#006496";
-			  format = "[ $symbol $branch ]($style)";
-			};
-
-			git_status = {
-			  style = "bg:\#006496";
-			  format = "[$all_status$ahead_behind ]($style)";
-			};
-		};
+       	gtk = {
+       	  gtk3 = {
+       	  	bookmarks = [
+       	  	  "file:///home/ymstnt/Documents"
+       	  	  "file:///home/ymstnt/Downloads"
+       	  	  "file:///home/ymstnt/Pictures"
+       	  	  "file:///home/ymstnt/Videos"
+       	  	  "file:///home/ymstnt/Music"
+       	  	  "file:///home/ymstnt/Documents/PROJEKTEK"
+       	  	  "file:///home/ymstnt/Documents/Iskola"
+       	  	  "file:///home/ymstnt/Documents/Munka"
+       	  	  "file:///etc/nixos nixos"
+       	  	  "sftp://ymstnt@ymstnt.com:42727/home/ymstnt/Files Shared Drive"
+       	  	];
+       	  };
+       	};
 	};
 }
