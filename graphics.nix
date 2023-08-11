98c0612ee0938cdac9ca751e880b597f4c0b40ee
YMSTNT <21342713+YMSTNT@users.noreply.github.com>
2023-08-11 22:59:08 +0200

    Split graphics into separate file

---
+++ graphics.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, ... }:
+
+{
+  # Nvidia
+  # Make sure opengl is enabled
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  # Tell Xorg to use the nvidia driver
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    # Modesetting is needed for most wayland compositors
+    modesetting.enable = true;
+    # Use the open source version of the kernel module
+    # Only available on driver 515.43.04+
+    open = true;
+    # Enable the nvidia settings menu
+    nvidiaSettings = true;
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    # Fix screen tearing
+    forceFullCompositionPipeline = true;
+    # Fix weird suspend
+    powerManagement.enable = true;
+  };
+}
