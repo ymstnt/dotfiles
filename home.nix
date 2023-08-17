16e2da5e268eccc2607b441653f708db82f3f38c
YMSTNT <21342713+YMSTNT@users.noreply.github.com>
2023-08-17 23:00:33 +0200

    Convert dotfiles into nix configs, delete old files

--- home.nix
+++ home.nix
@@ -9,6 +9,12 @@ in
     # your `configuration.nix` file.
     home.stateVersion = "23.05";
 
+    programs.bash = {
+      profileExtra = ''
+        export GTK_THEME=Adwaita:dark
+      '';
+    };
+
     programs.zsh = {
       enable = true;
 
@@ -25,11 +31,12 @@ in
 
       shellAliases = {
         ll = "ls -l";
-        update = "sudo nixos-rebuild switch";
+        update = "sudo nixos-rebuild switch -I nixos-config=$HOME/dotfiles/configuration.nix";
         channelupd = "sudo nix-channel --update";
-        nixconfig = "sudo micro /etc/nixos/configuration.nix";
-        homeconfig = "sudo micro /etc/nixos/home.nix";
+        nixconfig = "micro $HOME/dotfiles/configuration.nix";
+        homeconfig = "micro $HOME/dotfiles/home.nix";
         nixcd = "cd /etc/nixos";
+        dotcd = "cd $HOME/dotfiles";
         deletegarbage = "sudo nix-collect-garbage --delete-older-than 30d";
         zshreload = "source ~/.zshrc";
         lf = "ls -la | grep";
@@ -50,6 +57,11 @@ in
 
       enableAutosuggestions = true;
       enableSyntaxHighlighting = true;
+
+      profileExtra = ''
+        export GTK_THEME=Adwaita:dark # for GTK 3 and 4 apps
+        export QT_QPA_PLATFORMTHEME=gtk2 # for Qt 5 and 6 apps
+      '';
 
       initExtra = ''
         .  ~/.zshrc_old
@@ -98,6 +110,83 @@ in
         git_status = {
           style = "bg:\#006496";
           format = "[$all_status$ahead_behind ]($style)";
+        };
+      };
+    };
+
+    services.gammastep = {
+      enable = false; # Change this to true to enable Redshift
+      package = pkgs.redshift;
+
+      latitude = 47.68;
+      longitude = 19.09;
+
+      temperature = {
+        day = 5700;
+        night = 3500;
+      };
+
+      settings = {
+        general = {
+          fade = 1;
+          brightness-day = 1;
+          brightness-night = 0.85;
+          adjustment-method = "randr";
+        };
+      };
+    };
+
+    services.flameshot = {
+      enable = false; # Change this to true to enable Flameshot
+
+      settings = {
+        General = {
+          contrastOpacity = 188;
+          disabledTrayIcon = true;
+          drawColor = "#ff0e00";
+          drawFontSize = 8;
+          drawThickness = 3;
+          saveAfterCopy = true;
+          savePath = "/home/ymstnt/Pictures/Flameshot";
+          savePathFixed = true;
+          showHelp = false;
+          showStartupLaunchMessage = true;
+          startupLaunch = true;
+          undoLimit = 99;
+        };
+
+        Shortcuts = {
+          TYPE_ARROW = "A";
+          TYPE_CIRCLE = "C";
+          TYPE_CIRCLECOUNT = "";
+          TYPE_COMMIT_CURRENT_TOOL = "Ctrl+Return";
+          TYPE_COPY = "Ctrl+C";
+          TYPE_DELETE_CURRENT_TOOL = "Del";
+          TYPE_DRAWER = "D";
+          TYPE_EXIT = "Ctrl+Q";
+          TYPE_MARKER = "M";
+          TYPE_MOVESELECTION = "Ctrl+M";
+          TYPE_MOVE_DOWN = "Down";
+          TYPE_MOVE_LEFT = "Left";
+          TYPE_MOVE_RIGHT = "Right";
+          TYPE_MOVE_UP = "Up";
+          TYPE_OPEN_APP = "Ctrl+O";
+          TYPE_PENCIL = "P";
+          TYPE_PIN = "";
+          TYPE_PIXELATE = "B";
+          TYPE_RECTANGLE = "R";
+          TYPE_REDO = "Ctrl+Shift+Z";
+          TYPE_RESIZE_DOWN = "Shift+Down";
+          TYPE_RESIZE_LEFT = "Shift+Left";
+          TYPE_RESIZE_RIGHT = "Shift+Right";
+          TYPE_RESIZE_UP = "Shift+Up";
+          TYPE_SAVE = "Ctrl+S";
+          TYPE_SELECTION = "S";
+          TYPE_SELECTIONINDICATOR = "";
+          TYPE_SELECT_ALL = "Ctrl+A";
+          TYPE_TEXT = "T";
+          TYPE_TOGGLE_PANEL = "Space";
+          TYPE_UNDO = "Ctrl+Z";
         };
       };
     };
@@ -167,9 +256,20 @@ in
           "file:///home/ymstnt/Documents/PROJEKTEK"
           "file:///home/ymstnt/Documents/Iskola"
           "file:///home/ymstnt/Documents/Munka"
+          "file:///home/ymstnt/dotfiles dotfiles"
           "file:///etc/nixos nixos"
           "sftp://ymstnt@ymstnt.com:42727/home/ymstnt/Files Shared Drive"
         ];
+
+        extraConfig = {
+          gtk-application-prefer-dark-theme = 1;
+        };
+      };
+      gtk4 = {
+        extraConfig = {
+          gtk-hint-font-metrics = true;
+          gtk-application-prefer-dark-theme = 1;
+        };
       };
     };
 
@@ -184,3 +284,8 @@ in
     ];
   };
 }
+
+
+
+
+
