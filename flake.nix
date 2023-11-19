b4ab1d5e293c430ab7312dd672a9d76394ca0366
YMSTNT <21342713+YMSTNT@users.noreply.github.com>
2023-11-19 23:28:33 +0100

    Add "master" override

--- flake.nix
+++ flake.nix
@@ -9,6 +9,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-master.url = "github:NixOS/nixpkgs/master";
     nixpkgs-develop.url = "github:ymstnt/nixpkgs";
 
     home-manager = {
