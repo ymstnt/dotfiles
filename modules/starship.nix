cb86f84673b444560e5ffe97c4852e90fcbe64dd
YMSTNT <21342713+YMSTNT@users.noreply.github.com>
2024-01-26 18:57:46 +0100

    Starship: tweaked prompt color slightly

--- modules/starship.nix
+++ modules/starship.nix
@@ -6,24 +6,24 @@
   hm.programs.starship = {
     enable = true;
     settings = {
-      format = "[](\#003F5E)\$os\$username\[](bg:\#005077 fg:\#003F5E)\$directory\[](bg:\#006496 fg:\#005077)\$git_branch\$git_status\[ ](fg:\#006496)";
+      format = "[](\#2A3F6D)\$os\$username\[](bg:\#395694 fg:\#2A3F6D)\$directory\[](bg:\#466AB6 fg:\#395694)\$git_branch\$git_status\[ ](fg:\#466AB6)";
 
       username = {
         show_always = true;
-        style_user = "bg:\#003F5E";
-        style_root = "bg:\#003F5E";
+        style_user = "bg:\#2A3F6D";
+        style_root = "bg:\#2A3F6D";
         format = "[$user ]($style)";
         disabled = false;
       };
 
       os = {
-        format = "[ ]($style)";
-        style = "bg:\#003F5E";
+        format = "[ ]($style)";
+        style = "bg:\#2A3F6D";
         disabled = false;
       };
 
       directory = {
-        style = "bg:\#005077";
+        style = "bg:\#395694";
         format = "[ $path ]($style)";
         truncation_length = 3;
         truncation_symbol = "…/";
@@ -38,12 +38,12 @@
 
       git_branch = {
         symbol = "";
-        style = "bg:\#006496";
+        style = "bg:\#466AB6";
         format = "[ $symbol $branch ]($style)";
       };
 
       git_status = {
-        style = "bg:\#006496";
+        style = "bg:\#466AB6";
         format = "[$all_status$ahead_behind ]($style)";
       };
     };
