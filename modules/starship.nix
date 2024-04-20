fcaeba61cb933effb9961fe1b9e684a2321e966f
YMSTNT <21342713+YMSTNT@users.noreply.github.com>
2024-04-20 23:48:24 +0200

    starship: updated prompt - added command duration module - OS now shows the proper NF icon based on OS

--- modules/starship.nix
+++ modules/starship.nix
@@ -4,9 +4,10 @@
   hm.programs.starship = {
     enable = true;
     settings = {
-      command_timeout = 1000;
-      format = "[](\#2A3F6D)\$os\$username\[](bg:\#395694 fg:\#2A3F6D)\$directory\[](bg:\#466AB6 fg:\#395694)\$git_branch\$git_status\[ ](fg:\#466AB6)";
-
+      command_timeout = 5000;
+      format = "[](\#2A3F6D)\$os\$username\[](bg:\#395694 fg:\#2A3F6D)\$directory\[](bg:\#466AB6 fg:\#395694)\$git_branch\$git_status\[](bg:\#5374BD fg:\#466AB6)\$cmd_duration\[ ](fg:\#5374BD)"; # grayscale.design Tailwind
+      continuation_prompt = "▶▶ ";
+      
       username = {
         show_always = true;
         style_user = "bg:\#2A3F6D";
@@ -16,9 +17,51 @@
       };
 
       os = {
-        format = "[ ]($style)";
+        format = "[$symbol]($style)";
         style = "bg:\#2A3F6D";
         disabled = false;
+      };
+
+      os.symbols = {
+        AlmaLinux = " ";
+        Alpine = " ";
+        Amazon = " ";
+        Android = " ";
+        Arch = " ";
+        Artix = " ";
+        CentOS = " ";
+        Debian = " ";
+        DragonFly = " ";
+        EndeavourOS = " ";
+        Fedora = " ";
+        FreeBSD = " ";
+        Garuda = " ";
+        Gentoo = " ";
+        HardenedBSD = " ";
+        Illumos = " ";
+        Kali = " ";
+        Linux = " ";
+        Macos = " ";
+        Manjaro = " ";
+        MidnightBSD = " ";
+        Mint = "󰣭 ";
+        NetBSD = " ";
+        NixOS = " ";
+        OpenBSD = " ";
+        OpenCloudOS = " ";
+        openSUSE = " ";
+        OracleLinux = " ";
+        Pop = " ";
+        Raspbian = " ";
+        Redhat = "󱄛 ";
+        RedHatEnterprise = "󱄛 ";
+        RockyLinux = " ";
+        Solus = " ";
+        SUSE = " ";
+        Ubuntu = " ";
+        Unknown = " ";
+        Void = " ";
+        Windows = " ";
       };
 
       directory = {
@@ -44,6 +87,12 @@
       git_status = {
         style = "bg:\#466AB6";
         format = "[$all_status$ahead_behind ]($style)";
+      };
+
+      cmd_duration = {
+        disabled = false;
+        format = "[  $duration ]($style)";
+        style = "bg:\#5374BD";
       };
     };
   };
