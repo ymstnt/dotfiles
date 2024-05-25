2d16feae810d4da79185f7d6e122622191d222ea
YMSTNT <21342713+YMSTNT@users.noreply.github.com>
2024-05-25 15:57:21 +0200

    latex: init module - contains packages needed for Anki to function

---
+++ modules/latex.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }:
+
+{
+  hm.home.packages = with pkgs; [
+    (texlive.withPackages (ps: with ps; [
+      scheme-basic
+      latexmk
+      naive-ebnf
+      siunitx
+      steinmetz
+      circuitikz
+      collection-fontsrecommended
+      enumitem
+      environ
+      listings
+      pict2e
+      soul
+      titlesec
+      wrapfig
+      xstring
+      dvipng # needed for anki
+      algorithmicx
+      algorithms
+      algpseudocodex
+      alg
+      algxpar
+      algorithm2e
+    ]))
+    #texlive.combined.scheme-full
+  ];
+}
