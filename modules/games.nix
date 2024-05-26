c97222c5ac5f06efadeab3886489e6713d1f800a
YMSTNT <21342713+YMSTNT@users.noreply.github.com>
2024-05-27 00:22:21 +0200

    steam: added proton-ge and opened firewall ports

--- modules/games.nix
+++ modules/games.nix
@@ -2,7 +2,14 @@
 
 {
   services.ratbagd.enable = true;
-  programs.steam.enable = true;
+  programs.steam = {
+    enable = true;
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
+    remotePlay.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
 
   hm.home.packages = with pkgs; [
     prismlauncher
