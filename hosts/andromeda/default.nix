16280b3b4d60d8dd9b6ef515c8acb222344a501a
YMSTNT <21342713+YMSTNT@users.noreply.github.com>
2024-07-19 18:33:49 +0200

    andromeda: switch to open nvidia driver

--- hosts/andromeda/default.nix
+++ hosts/andromeda/default.nix
@@ -33,7 +33,7 @@
     modesetting.enable = true;
     # Use the open source version of the kernel module
     # Only available on driver 515.43.04+
-    open = false;
+    open = true;
     # Enable the nvidia settings menu
     nvidiaSettings = true;
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
