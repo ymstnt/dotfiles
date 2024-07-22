9146c41b1313957193b49f8ce5b68d06b23c7e5d
YMSTNT <21342713+YMSTNT@users.noreply.github.com>
2024-07-22 16:38:48 +0200

    cosmic: no longer a specialisation - move cosmic out of specialisation - cosmic: remove unneeded config

--- modules/cosmic.nix
+++ modules/cosmic.nix
@@ -1,29 +1,10 @@
-{ lib, pkgs, nixos-cosmic, ... }:
+{ nixos-cosmic, ... }:
 
 {
   imports = [ nixos-cosmic.nixosModules.default ];
 
-  specialisation.cosmic.configuration = {
-    services.xserver = {
-      displayManager.gdm.enable = lib.mkForce false;
-      desktopManager.gnome.enable = lib.mkForce false;
-    };
-    services = {
-      desktopManager.cosmic.enable = true;
-      displayManager.cosmic-greeter.enable = true;
-    };
-    fonts.packages = with pkgs; [
-      cantarell-fonts
-    ];
-    hm = {
-      dconf.settings = {
-        "org/gnome/desktop/wm/preferences" = {
-          button-layout = lib.mkForce "appmenu:minimize,maximize,close";
-        };
-      };
-      home.packages = (with pkgs; [
-        gnome-console
-      ]);
-    };
+  services = {
+    desktopManager.cosmic.enable = true;
+    displayManager.cosmic-greeter.enable = true;
   };
 }
