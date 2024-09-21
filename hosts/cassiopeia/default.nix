6a12627fac62da36490a2e67f64d800abf555634
YMSTNT <21342713+YMSTNT@users.noreply.github.com>
2024-09-21 10:53:52 +0200

    cassiopeia: remove firefox scaling fix

--- hosts/cassiopeia/default.nix
+++ hosts/cassiopeia/default.nix
@@ -40,7 +40,4 @@
 
   # Tell Xorg to use the modesetting driver
   services.xserver.videoDrivers = [ "modesetting" ];
-
-  # Firefox scaling fix
- hm.programs.firefox.profiles.dev-edition-default.settings."layout.css.devPixelsPerPx" = 1.15;  
 }
