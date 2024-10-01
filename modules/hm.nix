26b5fa883380266e3f4c2a3f1b9a7e65f59a90c0
YMSTNT <21342713+YMSTNT@users.noreply.github.com>
2024-10-01 23:09:20 +0200

    home-manager: resolve confilcts automatically

--- modules/hm.nix
+++ modules/hm.nix
@@ -6,8 +6,11 @@
     (lib.mkAliasOptionModule [ "hm" ] [ "home-manager" "users" "ymstnt" ])
   ];
 
-  home-manager.useGlobalPkgs = true;
-
+  home-manager = {
+    useGlobalPkgs = true;
+    backupFileExtension = "backup-<timestamp>";
+  };
+  
   home-manager.users.ymstnt = {
     home.stateVersion = config.system.stateVersion;
   };
