ed2a4f3416d4ba98e81280c3686c9a7850fa0635
YMSTNT <21342713+YMSTNT@users.noreply.github.com>
2024-10-22 17:51:10 +0200

    fonts: reorder and add twemoji font

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,20 +1,20 @@
 { pkgs, ... }:
 {
   fonts.packages = with pkgs; [
-    inter
-    noto-fonts
-    noto-fonts-cjk-serif
-    noto-fonts-cjk-sans
-    #google-fonts
-    noto-fonts-emoji
-    mplus-outline-fonts.githubRelease
-    proggyfonts
-    jetbrains-mono
-    corefonts
     cantarell-fonts
+    corefonts
     dejavu_fonts
+    inter
+    jetbrains-mono
+    mplus-outline-fonts.githubRelease
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+    noto-fonts-emoji
+    proggyfonts
     source-code-pro
     source-sans
+    twemoji-color-font
     (nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" "NerdFontsSymbolsOnly" ]; })
   ];
   # Enable fontDir for Flatpak
