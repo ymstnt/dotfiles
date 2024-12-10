709bfc356846be2ceb86b27340bbea291a78a187
YMSTNT <21342713+YMSTNT@users.noreply.github.com>
2024-12-10 23:27:17 +0100

    gnome-mutter: try fixing it

--- overlays/gnome-mutter.nix
+++ overlays/gnome-mutter.nix
@@ -2,7 +2,7 @@
 
 (final: prev: {
     mutter = prev.mutter.overrideAttrs (old: {
-      pathes = old.patches ++ [(pkgs.fetchpatch2 {
+      patches = (old.patches or []) ++ [(pkgs.fetchpatch2 {
         url = "https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/1441.diff";
         # GNOME 46: triple-buffering-v4-46
         hash = "sha256-en/4hui0zW8m8ZM2buZhr1FVMW3b9H1ZWKAB2018wmI=";
