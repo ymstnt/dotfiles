fe076ebb00e069af9a7bf94115ede1978b7d7ac3
ymstnt <21342713+ymstnt@users.noreply.github.com>
2025-01-24 00:08:14 +0100

    browser: removed vivaldi

--- modules/browser.nix
+++ modules/browser.nix
@@ -176,10 +176,6 @@
       };
     };
 
-    home.packages = with pkgs; [
-      vivaldi
-      vivaldi-ffmpeg-codecs
-    ];
     xdg.desktopEntries.firefox-devedition = {
       name = "Firefox Developer Edition";
       genericName = "Web Browser";
