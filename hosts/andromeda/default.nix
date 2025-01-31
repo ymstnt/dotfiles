37e56cd5e2a96f7b3a43f14d6366d4749be39a18
ymstnt <21342713+ymstnt@users.noreply.github.com>
2025-02-01 00:52:41 +0100

    andromeda: switch to cosmic, nvidia-open and beta

--- hosts/andromeda/default.nix
+++ hosts/andromeda/default.nix
@@ -4,7 +4,7 @@
   imports =
     [ ./hardware-configuration.nix ] ++
       self.nixosModules.allImportsExcept [
-        "cosmic"
+        "gnome"
         "pantheon"
       ];
 
@@ -33,11 +33,11 @@
     modesetting.enable = true;
     # Use the open source version of the kernel module
     # Only available on driver 515.43.04+
-    open = false;
+    open = true;
     # Enable the nvidia settings menu
     nvidiaSettings = true;
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
     # Fix screen tearing
     forceFullCompositionPipeline = true;
     # Fix weird suspend
