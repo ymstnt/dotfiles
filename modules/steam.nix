24d0b03fe1e35ad15177b16c3548883d8af4bf77
ymstnt <21342713+ymstnt@users.noreply.github.com>
2025-02-01 00:52:57 +0100

    steam: enable gamescope and gamemode

--- modules/steam.nix
+++ modules/steam.nix
@@ -10,5 +10,12 @@
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
+    gamescopeSession = {
+      enable = true;
+    };
   };
+
+  programs.gamescope.enable = true;
+
+  programs.gamemode.enable = true;
 }
