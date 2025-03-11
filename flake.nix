fb87b30ac1fcb30224ce27a65fd1c559ddf74dc3
ymstnt <21342713+ymstnt@users.noreply.github.com>
2025-03-12 00:14:44 +0100

    flake(input update): updated nixpkgs-stable to 24.11

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-master.url = "github:NixOS/nixpkgs/master";
     nixpkgs-develop.url = "github:ymstnt/nixpkgs/master";
     lix-module = {
