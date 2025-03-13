2addde0d5661baa8645322f6a2a80a67d8db53fd
ymstnt <21342713+ymstnt@users.noreply.github.com>
2025-03-13 22:36:12 +0100

    thunderbird(modify pkg): use thunderbird-latest

--- modules/thunderbird.nix
+++ modules/thunderbird.nix
@@ -3,7 +3,7 @@
 {
   hm.programs.thunderbird = {
     enable = true;
-    package = pkgs.thunderbird;
+    package = pkgs.thunderbird-latest;
     profiles.main = {
       isDefault = true;
       userChrome = ''
