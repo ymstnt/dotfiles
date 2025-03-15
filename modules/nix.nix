cffcd2ea1252eb8b3866a927ff2a301d9fd8d605
ymstnt <21342713+ymstnt@users.noreply.github.com>
2025-03-15 19:37:48 +0100

    nix: added nix-community cache

--- modules/nix.nix
+++ modules/nix.nix
@@ -10,8 +10,14 @@
     ];
     max-jobs = 1;
     trusted-users = [ "ymstnt" ];
-    substituters = [ "https://cosmic.cachix.org/" ];
-    trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+    substituters = [
+      "https://cosmic.cachix.org/"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
 
   nix.gc = {
