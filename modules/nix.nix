10a5bf72346832d8c7562048a0779802fb383eb9
ymstnt <21342713+ymstnt@users.noreply.github.com>
2025-04-17 16:11:42 +0200

    feat(nix): add gepbird's cachix

--- modules/nix.nix
+++ modules/nix.nix
@@ -17,6 +17,7 @@
     trusted-public-keys = [
       "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "gepbird-nur-packages.cachix.org-1:Ip2iveknanFBbJ2DFWk8cDomfRquUJiMWS/2fSeuMis="
     ];
   };
 
