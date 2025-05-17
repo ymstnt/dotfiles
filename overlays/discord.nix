393c5d1ac0cd20deabdeab98c0ec217ee59942fd
ymstnt <21342713+ymstnt@users.noreply.github.com>
2025-05-17 15:00:17 +0200

    discord(fix): override vencord to use pkg from master

--- overlays/discord.nix
+++ overlays/discord.nix
@@ -1,8 +1,9 @@
-{ ... }:
+{ nixpkgs-master, ... }:
 
 (final: prev: {
 	discord = prev.discord.override {
 		withOpenASAR = true;
 		withVencord = true;
+		vencord = nixpkgs-master.legacyPackages.${final.system}.vencord;
 	};
 })
