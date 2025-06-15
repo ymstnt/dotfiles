36febf18d501d688c6c5850fca92e296241a754c
ymstnt <21342713+ymstnt@users.noreply.github.com>
2025-06-16 00:45:32 +0200

    feat(agenix): automate secret declaration

--- modules/agenix.nix
+++ modules/agenix.nix
@@ -1,12 +1,30 @@
-{ agenix, ... }:
+{ pkgs, agenix, ... }:
 
+let
+  inherit (builtins)
+    attrNames
+    listToAttrs
+    readDir
+    removeAttrs
+    replaceStrings
+    ;
+
+  secretsFolder = toString ../secrets;
+  secretFileNames = attrNames (removeAttrs (readDir secretsFolder) [ "secrets.nix" ]);
+
+  secrets = listToAttrs (
+    map (fileName: {
+      name = replaceStrings [ ".age" ] [ "" ] fileName;
+      value = {
+        file = secretsFolder + "/" + fileName;
+      };
+    }) secretFileNames
+  );
+
+in
 {
   imports = [ agenix.nixosModules.default ];
-  hm.home.packages = [ agenix.packages.x86_64-linux.default ];
+  environment.systemPackages = [ agenix.packages.${pkgs.system}.default ];
 
-  age = {
-    secrets = {
-    };
-  };
-
+  age.secrets = secrets;
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,12 +1,30 @@
 let
-  keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG0MpAJw5Q5t4yaxX88Xr1cUjMHEfjcfq++v7efoqTIa root@cassiopeia"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAHfr8D6mA/MhI/5CQF/NiVCBiwWheLog31vpFmXtIu8 root@andromeda"
+  inherit (builtins)
+    attrNames
+    listToAttrs
+    readDir
+    removeAttrs
+    ;
 
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDVor+g/31/XFIzuZYQrNK/RIbU1iDaSyOfM8re73eAd ymstnt@cassiopeia"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEVxinYyV/gDhWNeSa0LD6kRKwTWhFxXVS23axGO/2sa ymstnt@andromeda"
+  cassiopeia-root = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG0MpAJw5Q5t4yaxX88Xr1cUjMHEfjcfq++v7efoqTIa";
+  andromeda-root = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAHfr8D6mA/MhI/5CQF/NiVCBiwWheLog31vpFmXtIu8";
+  cassiopeia = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDVor+g/31/XFIzuZYQrNK/RIbU1iDaSyOfM8re73eAd";
+  andromeda = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEVxinYyV/gDhWNeSa0LD6kRKwTWhFxXVS23axGO/2sa";
+
+  keys = [
+    cassiopeia-root
+    andromeda-root
+    cassiopeia
+    andromeda
   ];
+  secretFiles = attrNames (removeAttrs (readDir (toString ./.)) [ "secrets.nix" ]);
+  result = listToAttrs (
+    map (x: {
+      name = x;
+      value = {
+        publicKeys = keys;
+      };
+    }) secretFiles
+  );
 in
-{
-  
-}
+result
