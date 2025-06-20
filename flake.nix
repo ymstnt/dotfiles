4a104d2960ceb9086e5a57234eecfec6c230db94
ymstnt <21342713+ymstnt@users.noreply.github.com>
2025-06-20 23:50:27 +0200

    fix(gui): patch plex-desktop

--- flake.nix
+++ flake.nix
@@ -36,6 +36,10 @@
       url = "gitlab:doronbehar/nix-matlab";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixpkgs-patch-plex-desktop-fix-build = {
+      url = "https://github.com/NixOS/nixpkgs/pull/418546.diff";
+      flake = false;
+    };
   };
 
   outputs = { self, nixpkgs, nixpkgs-patcher, lix-module, home-manager, cosmic-manager, ... } @inputs: {
