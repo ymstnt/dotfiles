bd27d38f2343b99c8f6d2c7dec02c3f47f3e1683
ymstnt <21342713+ymstnt@users.noreply.github.com>
2025-07-04 22:43:55 +0200

    Revert "discord(fix): override vencord to use pkg from master"
    This reverts commit 393c5d1ac0cd20deabdeab98c0ec217ee59942fd.

--- overlays/discord.nix
+++ overlays/discord.nix
@@ -1,9 +1,8 @@
-{ nixpkgs-master, ... }:
+{ ... }:
 
 (final: prev: {
 	discord = prev.discord.override {
 		withOpenASAR = true;
 		withVencord = true;
-		vencord = nixpkgs-master.legacyPackages.${final.system}.vencord;
 	};
 })
