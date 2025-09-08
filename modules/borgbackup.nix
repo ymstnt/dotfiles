bbadef0b3f60d271b488f590695e3f0da192e94d
ymstnt <21342713+ymstnt@users.noreply.github.com>
2025-09-08 18:36:08 +0200

    feat(borgbackup): add andromeda, correct path

--- modules/borgbackup.nix
+++ modules/borgbackup.nix
@@ -3,9 +3,10 @@
 {
   services.borgbackup.repos = {
     backup-vela-ymstnt = {
-      path = "/run/media/ymstnt/TNT-PSPRT";
+      path = "/run/media/ymstnt/TNT-PSPRT/backup-vela-ymstnt";
       authorizedKeys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKLQKmZDSyZvpXqaqLigdrQEJzrcu4ry0zGydZipliPZ u0_a293@localhost" # vela
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEVxinYyV/gDhWNeSa0LD6kRKwTWhFxXVS23axGO/2sa ymstnt@andromeda"
       ];
     };
   };
