58fad8a991b580915803170f54a46f3c8394e4a9
ymstnt <21342713+ymstnt@users.noreply.github.com>
2025-10-06 18:09:30 +0200

    feat(flake): add patch for avahi debug

--- flake.nix
+++ flake.nix
@@ -36,6 +36,10 @@
       url = "gitlab:doronbehar/nix-matlab";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixpkgs-patch-avahi-debug = {
+      url = "https://github.com/NixOS/nixpkgs/pull/448254.diff";
+      flake = false;
+    };
   };
 
   outputs = { self, nixpkgs, nixpkgs-patcher, lix-module, home-manager, cosmic-manager, ... } @inputs: {
