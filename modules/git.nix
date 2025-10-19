043746227ad4623377ede755a156bbcda40438b2
ymstnt <21342713+ymstnt@users.noreply.github.com>
2025-10-19 19:52:25 +0200

    chore(git): rename settings

--- modules/git.nix
+++ modules/git.nix
@@ -5,17 +5,19 @@
     programs = {
       git = {
         enable = true;
-        userName = "ymstnt";
-        userEmail = "21342713+ymstnt@users.noreply.github.com";
-        aliases = {
-          s = "status -uno";
-          d = "diff";
-          c = "commit -m";
-          p = "!git pull && git push";
-          br = "branch --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(contents:subject) %(color:green)(%(committerdate:relative)) [%(authorname)]' --sort=-committerdate";
-          lg = "!git log --pretty=format:\'%C(magenta)%h%Creset -%C(red)%d%Creset %s %C(dim green)(%cr) [%an]\' --abbrev-commit -30";
-        };
-        extraConfig = {
+        settings = {
+          user = {
+            name = "ymstnt";
+            email = "21342713+ymstnt@users.noreply.github.com";
+          };
+          alias = {
+            s = "status -uno";
+            d = "diff";
+            c = "commit -m";
+            p = "!git pull && git push";
+            br = "branch --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(contents:subject) %(color:green)(%(committerdate:relative)) [%(authorname)]' --sort=-committerdate";
+            lg = "!git log --pretty=format:\'%C(magenta)%h%Creset -%C(red)%d%Creset %s %C(dim green)(%cr) [%an]\' --abbrev-commit -30";
+          };
           init.defaultBranch = "main";
           core.editor = "hx";
           push.autoSetupRemote = true;
