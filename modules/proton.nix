f293c4e8f1aa6873d7a5ca13d8d1a33c288e916c
ymstnt <21342713+ymstnt@users.noreply.github.com>
2025-11-11 09:18:39 +0100

    chore(proton): add instructions for protonmail-bridge

--- modules/proton.nix
+++ modules/proton.nix
@@ -6,6 +6,10 @@
     protonmail-desktop
   ];
 
+  # Configure it with protonmail-bridge --cli
+  # Then exit, and systemctl --user enable protonmail-bridge.service
+  # systemctl --user start protonmail-bridge.service
+  # If needed to reconfigure, systemctl --user stop protonmail-bridge.service
   services.protonmail-bridge = {
     enable = true;
     path = [ pkgs.gnome-keyring ];
