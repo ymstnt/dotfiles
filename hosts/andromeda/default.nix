bd8ec7043cbe4af80e86fec179ef2bd9ff15027e
ymstnt <21342713+ymstnt@users.noreply.github.com>
2025-12-13 21:48:01 +0100

    feat(andromeda): switch to closed source nvidia drivers

--- hosts/andromeda/default.nix
+++ hosts/andromeda/default.nix
@@ -41,7 +41,7 @@
     modesetting.enable = true;
     # Use the open source version of the kernel module
     # Only available on driver 515.43.04+
-    open = true;
+    open = false;
     # Enable the nvidia settings menu
     nvidiaSettings = true;
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
