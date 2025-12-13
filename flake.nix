1fee4b76e53cec7d9eda9ba9fcd0a8d0af256f8d
ymstnt <21342713+ymstnt@users.noreply.github.com>
2025-12-13 21:47:46 +0100

    chore(flake): remove obsolete rquickshare patch

--- flake.nix
+++ flake.nix
@@ -36,10 +36,6 @@
       url = "gitlab:doronbehar/nix-matlab";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs-patch-rquickshare = {
-      url = "https://github.com/NixOS/nixpkgs/pull/468481.diff";
-      flake = false;
-    };
   };
 
   outputs = { self, nixpkgs, nixpkgs-patcher, lix-module, home-manager, cosmic-manager, ... } @inputs: {
