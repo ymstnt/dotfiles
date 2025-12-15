e5bc89ccdc8e1e13d85845190a5b5a01cf7aec5a
ymstnt <21342713+ymstnt@users.noreply.github.com>
2025-12-15 18:27:49 +0100

    feat(nix): add tchfoo nix-cache

--- modules/nix.nix
+++ modules/nix.nix
@@ -13,11 +13,13 @@
     substituters = [
       "https://cosmic.cachix.org/"
       "https://nix-community.cachix.org"
+      "https://nix-cache.tchfoo.com"
     ];
     trusted-public-keys = [
       "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "gepbird-nur-packages.cachix.org-1:Ip2iveknanFBbJ2DFWk8cDomfRquUJiMWS/2fSeuMis="
+      "nix-cache.tchfoo.com-1:pWK4l0phRA3bE0CviZodEQ5mWAQYoiuVi2LML+VNtNY="
     ];
   };
 
