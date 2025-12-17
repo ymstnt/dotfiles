f564b00af205ebebf372ea4fb380d42093eb5116
ymstnt <21342713+ymstnt@users.noreply.github.com>
2025-12-17 10:47:01 +0100

    Revert "feat(flake): patch cosmic to stable"
    This reverts commit 9c2f73ab945b096e29d0cf71c57568dc1ca9d1d5.

--- flake.nix
+++ flake.nix
@@ -36,10 +36,6 @@
       url = "gitlab:doronbehar/nix-matlab";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs-patch-cosmic-stable = {
-      url = "https://github.com/NixOS/nixpkgs/pull/469745.diff";
-      flake = false;
-    };
   };
 
   outputs = { self, nixpkgs, nixpkgs-patcher, lix-module, home-manager, cosmic-manager, ... } @inputs: {
