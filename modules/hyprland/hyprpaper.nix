6fe4d75d1ee21f1f2c6b2b3367d2cb1e0169df04
ymstnt <21342713+ymstnt@users.noreply.github.com>
2026-01-25 00:03:00 +0100

    feat(hyprpaper): init module

---
+++ modules/hyprland/hyprpaper.nix
@@ -0,0 +1,20 @@
+{ ... }:
+
+{
+  hm.services.hyprpaper = {
+    enable = true;
+    settings = {
+      splash = false;
+      wallpaper = [
+        {
+          monitor = "DP-3";
+          path = "$HOME/Pictures/Wallpapers/paul-volkmer-qVotvbsuM_c-unsplash.jpg";
+        }
+        {
+          monitor = "HDMI-A-1";
+          path = "$HOME/Pictures/Wallpapers/paul-volkmer-qVotvbsuM_c-unsplash.jpg";
+        }
+      ];
+    };
+  };
+}
