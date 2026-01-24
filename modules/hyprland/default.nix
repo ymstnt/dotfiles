6ff20d5915f5cc39d2afe29c1f23a9d337dceedc
ymstnt <21342713+ymstnt@users.noreply.github.com>
2026-01-25 00:04:38 +0100

    feat(hyprland): use subfolder for related modules

---
+++ modules/hyprland/default.nix
@@ -0,0 +1,16 @@
+{ ... }:
+
+{
+  imports = [
+    ./ashell.nix
+    ./hyprland.nix
+    ./hyprlauncher.nix
+    ./hyprlock.nix
+    ./hyprpaper.nix
+    ./hyprshot.nix
+    ./hyprsunset.nix
+    ./portals.nix
+    ./swaync.nix
+    ./tofi.nix
+  ];
+}
