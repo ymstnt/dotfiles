315c75dacc5f8cefaac0881a2e2800162a05ae0f
ymstnt <21342713+ymstnt@users.noreply.github.com>
2026-01-25 00:04:08 +0100

    feat(ashell): init module, use on hyprland

---
+++ modules/hyprland/ashell.nix
@@ -0,0 +1,38 @@
+{ ... }:
+
+{
+  hm.programs.ashell = {
+    enable = true;
+    systemd.enable = true;
+    settings = {
+      modules = {
+        center = [
+          "Clock"
+        ];
+        left = [
+          "AppLauncher"
+          "Workspaces"
+          "WindowTitle"
+        ];
+        right = [
+          "SystemInfo"
+          [
+            "Tray"
+            "Privacy"
+            "Settings"
+          ]
+        ];
+      };
+      app_launcher_cmd = "tofi-drun | xargs uwsm app --";
+      workspaces = {
+        visibility_mode = "MonitorSpecific";
+      };
+      window_title = {
+        truncate_title_after_length = 100;
+      };
+      settings = {
+        lock_cmd = "hyprlock &";
+      };
+    };
+  };
+}
