35dd1c7c934c63549412d2c0e5708735dd162634
ymstnt <21342713+ymstnt@users.noreply.github.com>
2026-02-01 01:03:27 +0100

    fix(kdenlive): fix build failure

--- overlays/kdenlive.nix
+++ overlays/kdenlive.nix
@@ -1,18 +1,26 @@
-{ ... }:
+{ nixpkgs-kdenlive, ... }:
 
-(final: prev: {
-  kdePackages = prev.kdePackages.overrideScope (
-    kdeFinal: kdePrev: {
-      kdenlive = prev.symlinkJoin {
-        name = "kdenlive-with-plugins-${kdePrev.kdenlive.version}";
-        paths = [ kdePrev.kdenlive ];
-        nativeBuildInputs = [
-          prev.makeBinaryWrapper
-        ];
-        postBuild = ''
-          wrapProgram $out/bin/kdenlive --prefix LADSPA_PATH : ${prev.rnnoise-plugin}/lib/ladspa
-        '';
-      };
-    }
-  );
-})
+(
+  final: prev:
+  let
+    pkgs-kdenlive = import nixpkgs-kdenlive {
+      inherit (prev.stdenv.hostPlatform) system;
+    };
+  in
+  {
+    kdePackages = prev.kdePackages.overrideScope (
+      kdeFinal: kdePrev: {
+        kdenlive = prev.symlinkJoin {
+          name = "kdenlive-with-plugins-${kdePrev.kdenlive.version}";
+          paths = [ pkgs-kdenlive.kdePackages.kdenlive ];
+          nativeBuildInputs = [
+            prev.makeBinaryWrapper
+          ];
+          postBuild = ''
+            wrapProgram $out/bin/kdenlive --prefix LADSPA_PATH : ${prev.rnnoise-plugin}/lib/ladspa
+          '';
+        };
+      }
+    );
+  }
+)
