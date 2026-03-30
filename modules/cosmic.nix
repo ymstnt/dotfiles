9f1020bd7822a10a519f4e4d13703ca483ec056e
ymstnt <21342713+ymstnt@users.noreply.github.com>
2026-03-30 11:03:45 +0200

    chore(cosmic): specify gtk4 theme, fixes eval warning

--- modules/cosmic.nix
+++ modules/cosmic.nix
@@ -87,7 +87,10 @@ in
     gtk = {
       enable = true;
       gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
-      gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
+      gtk4 = {
+        theme = null;
+        extraConfig.gtk-application-prefer-dark-theme = 1;
+      };
     };
     home.packages = (with pkgs; [
         forecast
