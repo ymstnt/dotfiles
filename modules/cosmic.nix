1da01154e51bd3d40ae1c62ea1f1a85bf107a993
ymstnt <21342713+ymstnt@users.noreply.github.com>
2026-05-19 10:05:50 +0200

    feat(cosmic): pin brave origin beta instead of nightly

--- modules/cosmic.nix
+++ modules/cosmic.nix
@@ -24,7 +24,7 @@ in
       applets = {
         app-list.settings = {
           favorites = [
-            "brave-origin-nightly"
+            "brave-origin-beta"
             "thunderbird"
             "discord"
             "TeamSpeak"
