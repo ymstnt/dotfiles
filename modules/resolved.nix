317e5b436d6af51b12a0a7725920ae5a9a8358d9
ymstnt <21342713+ymstnt@users.noreply.github.com>
2026-06-19 22:36:03 +0200

    feat(resolved): simplify config

--- modules/resolved.nix
+++ modules/resolved.nix
@@ -1,28 +1,12 @@
 { config, ... }:
 
 {
-  sops.templates."controld-dns.conf" = {
-    content = ''
-      [Resolve]
-      DNS=${config.sops.placeholder."dns/${config.networking.hostName}"}
-      Domains=~.
-    '';
-    owner = "systemd-resolve";
-    group = "systemd-resolve";
+  sops.secrets."dns/${config.networking.hostName}" = {
+    path = "/etc/systemd/resolved.conf.d/controld.conf";
+    owner = "systemd-resolve" ;
+    group = "systemd-resolve" ;
     restartUnits = [ "systemd-resolved.service" ];
   };
-
-  systemd.services.systemd-resolved.wantedBy = [ "sops-secrets-populator.service" ];
-  systemd.services.systemd-resolved.after = [ "sops-secrets-populator.service" ];
-
-  systemd.tmpfiles.rules = [
-    "d /run/systemd/resolved.conf.d 0755 root root -"
-    "L+ /run/systemd/resolved.conf.d/controld.conf - - - - ${config.sops.templates."controld-dns.conf".path}"
-  ];
-
-  systemd.services.systemd-resolved.restartTriggers = [
-    config.sops.templates."controld-dns.conf".file
-  ];
 
   services.resolved = {
     enable = true;
