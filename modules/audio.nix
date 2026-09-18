07592d39eb51c3c07aff2081032d09a36153f8ed
ymstnt <ymstnt@ymstnt.com>
2026-09-19 00:36:44 +0200

    feat(audio): add workaround for lewitt connect 2 input bug - Lewitt CONNECT 2 is exposed as a 4-channel stream with 2 channels recording the desktop audio - This workaround adds a "clean" input which is a version of the original input source with those 2 extra channels removed

--- modules/audio.nix
+++ modules/audio.nix
@@ -9,6 +9,28 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    extraConfig.pipewire = {
+      "99-lewitt-mic.conf" = {
+        "context.modules" = [
+          {
+            name = "libpipewire-module-loopback";
+            args = {
+              "node.description" = "Microphone - CONNECT 2 (Clean)";
+              "capture.props" = {
+                "node.target" = "alsa_input.lewitt_connect_2";
+                "audio.position" = [ "FL" "FR" ];
+                "stream.dont-remix" = true;
+                "node.passive" = true;
+              };
+              "playback.props" = {
+                "media.class" = "Audio/Source";
+                "audio.position" = [ "FL" "FR" ];
+              };
+            };
+          }
+        ];
+      };
+    };
     wireplumber.extraConfig = {
       "disable-monitor-sounds" = {
         "monitor.alsa.rules" = [
@@ -21,6 +43,20 @@
             actions = {
               update-props = {
                 device.disabled = true;
+              };
+            };
+          }
+        ];
+      };
+      "99-lewitt-rename" = {
+        "monitor.alsa.rules" = [
+          {
+            matches = [
+              { "node.name" = "~alsa_input.usb-Lewitt_GmbH_CONNECT_2_.*"; }
+            ];
+            actions = {
+              update-props = {
+                "node.name" = "alsa_input.lewitt_connect_2";
               };
             };
           }
